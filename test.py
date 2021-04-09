328d1bff180be918c629ddaf50c9a2bbd0b57c50
MinaToma <minatoma1999@gmail.com>
2021-04-09 16:13:03 +0200

    added ssim to test

--- test.py
+++ test.py
@@ -61,15 +61,14 @@ def eval():
     
     if not opt.upscale_only:
         avg_psnr_predicted = 0.0
+        avg_ssim_predicted = 0.0
 
     for batch in testing_data_loader:
         input, target = batch[0], batch[1]
 
         with torch.no_grad():
-            if cuda:
-                neigbor = neigbor.cuda()
-            else:
-                neigbor = [Variable(j).to(device=device, dtype=torch.float) for j in neigbor]
+            input = input.to(device)
+            target = target.to(device)
 
         t0 = time.time()
         with torch.no_grad():
@@ -87,13 +86,17 @@ def eval():
         target = target.squeeze().numpy().astype(np.float32)
         target = target * 255.
         if not upscale_only:
-            psnr_predicted = PSNR(prediction, targe)
+            psnr_predicted = PSNR(prediction, target)
+            ssim_predicted = SSIM(prediction, target)
             print("PSNR Predicted = ", psnr_predicted)
+            print("SSIM Predicted = ", ssim_predicted)
             avg_psnr_predicted += psnr_predicted
+            avg_ssim_predicted += ssim_predicted
         count += 1
     
     if not upscale_only:
         print("Avg PSNR Predicted = ", avg_psnr_predicted / count)
+        print("Avg SSIM Predicted = ", avg_ssim_predicted / count)
 
 
 def save_img(img, img_name, pred_flag):
@@ -120,6 +123,75 @@ def PSNR(pred, gt, shave_border=4):
     if rmse == 0:
         return 100
     return 20 * math.log10(255.0 / rmse)
+
+def _ssim(img1, img2):
+    """Calculate SSIM (structural similarity) for one channel images.
+
+    It is called by func:`calculate_ssim`.
+
+    Args:
+        img1 (ndarray): Images with range [0, 255] with order 'HWC'.
+        img2 (ndarray): Images with range [0, 255] with order 'HWC'.
+
+    Returns:
+        float: ssim result.
+    """
+
+    C1 = (0.01 * 255)**2
+    C2 = (0.03 * 255)**2
+
+    img1 = img1.astype(np.float64)
+    img2 = img2.astype(np.float64)
+    kernel = cv2.getGaussianKernel(11, 1.5)
+    window = np.outer(kernel, kernel.transpose())
+
+    mu1 = cv2.filter2D(img1, -1, window)[5:-5, 5:-5]
+    mu2 = cv2.filter2D(img2, -1, window)[5:-5, 5:-5]
+    mu1_sq = mu1**2
+    mu2_sq = mu2**2
+    mu1_mu2 = mu1 * mu2
+    sigma1_sq = cv2.filter2D(img1**2, -1, window)[5:-5, 5:-5] - mu1_sq
+    sigma2_sq = cv2.filter2D(img2**2, -1, window)[5:-5, 5:-5] - mu2_sq
+    sigma12 = cv2.filter2D(img1 * img2, -1, window)[5:-5, 5:-5] - mu1_mu2
+
+    ssim_map = ((2 * mu1_mu2 + C1) *
+                (2 * sigma12 + C2)) / ((mu1_sq + mu2_sq + C1) *
+                                       (sigma1_sq + sigma2_sq + C2))
+    return ssim_map.mean()
+
+
+def SSIM(img1, img2):
+    """Calculate SSIM (structural similarity).
+
+    Ref:
+    Image quality assessment: From error visibility to structural similarity
+
+    The results are the same as that of the official released MATLAB code in
+    https://ece.uwaterloo.ca/~z70wang/research/ssim/.
+
+    For three-channel images, SSIM is calculated for each channel and then
+    averaged.
+
+    Args:
+        img1 (ndarray): Images with range [0, 255].
+        img2 (ndarray): Images with range [0, 255].
+        crop_border (int): Cropped pixels in each edge of an image. These
+            pixels are not involved in the SSIM calculation.
+        input_order (str): Whether the input order is 'HWC' or 'CHW'.
+            Default: 'HWC'.
+        test_y_channel (bool): Test on Y channel of YCbCr. Default: False.
+
+    Returns:
+        float: ssim result.
+    """
+
+    img1 = img1.astype(np.float64)
+    img2 = img2.astype(np.float64)
+
+    ssims = []
+    for i in range(img1.shape[2]):
+        ssims.append(_ssim(img1[..., i], img2[..., i]))
+    return np.array(ssims).mean()
 
 if __name__ == "__main__":
     eval()
