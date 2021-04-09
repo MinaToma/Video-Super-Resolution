e24fadf12475546fe0651f50627b202eae56ad7a
MinaToma <minatoma1999@gmail.com>
2021-04-09 17:37:20 +0200

    add reds as test set

--- dataset.py
+++ dataset.py
@@ -12,6 +12,8 @@ def get_training_set(opt):
     return REDSTrainDataset(opt)
 
 def get_test_set(opt):
+    if opt.dataset_name == 'REDS':
+        return REDSTestDataset(opt)
     return Vid4TestDataset(opt)
 
 class DataAug(object):
@@ -76,6 +78,48 @@ class REDSTrainDataset(data.Dataset):
         frames_lr = np.zeros((self.frame_num, int(h / self.scale), int(w / self.scale), ch))
         for j in range(center_index - self.half_frame_num, center_index + self.half_frame_num + 1):
             i = j - center_index + self.half_frame_num
+            frames_lr_name = '{}/{}/{}'.format(self.dir_LR, folder_name, self.img_list[j])
+            img = cv2.imread(frames_lr_name)
+            frames_lr[i, :, :, :] = img  # t h w c
+
+        sample = {'lr': frames_lr, 'hr': frames_hr}
+        sample = self.transform(sample)
+
+        return sample['lr'], sample['hr']
+
+class REDSTestDataset(data.Dataset):
+    def __init__(self, opt):
+        self.dir_HR = opt.gt_dir
+        self.dir_LR = opt.lr_dir
+        self.dir_lis = sorted(listdir(self.dir_HR))
+        self.img_list = sorted(listdir('{}/{}/'.format(self.dir_HR, self.dir_lis[0])))
+        self.frame_num = opt.frame
+        self.half_frame_num = int(self.frame_num / 2)
+        self.transform = Compose([ToTensor()])
+        self.scale = 4
+        self.len = len(self.dir_lis) * len(self.img_list)
+
+    def __len__(self):
+        return self.len
+
+    def __getitem__(self, idx):
+        folder_index = idx // len(self.img_list)
+        folder_name = self.dir_lis[folder_index]
+        hr_folder_path = '{}/{}'.format(self.dir_HR, folder_name)
+
+        center_index = (idx % len(self.img_list))
+
+        frames_hr_name = '{}/{}'.format(hr_folder_path, self.img_list[center_index])
+        frames_hr = cv2.imread(frames_hr_name)
+        h, w, ch = frames_hr.shape
+
+        frames_lr = np.zeros((self.frame_num, int(h / self.scale), int(w / self.scale), ch))
+        for j in range(center_index - self.half_frame_num, center_index + self.half_frame_num + 1):
+            i = j - center_index + self.half_frame_num
+            if j < 0: 
+                j = 0
+            if j >= self.len:
+                j = self.len - 1
             frames_lr_name = '{}/{}/{}'.format(self.dir_LR, folder_name, self.img_list[j])
             img = cv2.imread(frames_lr_name)
             frames_lr[i, :, :, :] = img  # t h w c
